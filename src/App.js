3432c27e9c041dc6e29ee6aeeaaf2f2718b2d489
slmatthew <mc-matuhak@ya.ru>
2019-02-06 23:16:48 +0300

    Переработка приложения

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import connect from '@vkontakte/vkui-connect';
-import { View, ActionSheet, ActionSheetItem, platform, IOS } from '@vkontakte/vkui';
+import { Root, View } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
+import MainHome from './panels/Home/Main';
 
-const settings = require('./settings');
+import Adding from './panels/Adding/Main';
+
+const allowed = {
+	main: ['home', 'persik'],
+	adding: ['home'],
+};
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			activePanel: 'home',
-			fetchedUser: null,
-			search: '',
-			hasPasswords: localStorage.passwords ? true: false,
-			passwords: localStorage.passwords ? JSON.parse(localStorage.passwords) : null,
-			valid: null,
-			popout: null
+			activeView: 'main',
+			mainPanel: 'home',
+			addingPanel: 'home',
+			hacked: false,
+			fetchedUser: null
 		};
 
-		this.onSearch = this.onSearch.bind(this);
-		this.openAbout = this.openAbout.bind(this);
-		this.openActionSheet = this.openActionSheet.bind(this);
+		this.go = this.go.bind(this);
+		this.changeView = this.changeView.bind(this);
 	}
 
 	componentDidMount() {
@@ -38,83 +39,33 @@ class App extends React.Component {
 			}
 		});
 		connect.send('VKWebAppGetUserInfo', {});
-		this.setState({ valid: this.checkSign() });
 	}
 
-	go = (e) => {
-		this.setState({ activePanel: e.currentTarget.dataset.to })
-	};
-
-	onSearch(text) { this.setState({ search: text }); }
-
-	get passwords() {
-		if(this.state.passwords === null) return [];
-
-		const search = this.state.search.toLowerCase();
-		return this.state.passwords.filter(({login}) => login.toLowerCase().indexOf(search) > -1);
+	go(p) {
+		if(!allowed[this.state.activeView] || !allowed[this.state.activeView].includes(p)) {
+			this.setState({ hacked: true, activeView: 'hacked' });
+		} else {
+			if(this.state.activeView === 'main') {
+				this.setState({ mainPanel: p });
+			}
+		}
 	}
-
-	checkSign() {
-			if(!window.location.search) return false;
-			
-			const crypto = require('crypto');
-
-			let params = window.location.search.replace('?', '').split('&'),
-					arr = [];
-
-			params.forEach((param, i) => {
-				if(param.indexOf('vk_') !== -1) arr[i] = param;
-			});
-
-			arr = arr.sort().join('&');
-
-			let hash = crypto.createHmac('sha256', settings.client_secret).update(arr).digest('base64'),
-					sign = hash.replace(/\//g, '_').replace(/=/g, ''),
-					vk_sign = window.location.search.match(new RegExp('sign=([^&=]+)'))[1] || '';
-					
-			console.log(`sign checking result:`, sign === vk_sign);
-			if(sign !== vk_sign) console.warn(`sign isn't valid`);
-
-			return (sign === vk_sign);
-	};
-
-	openAbout() { this.setState({ activePanel: 'persik' }); }
-
-	openActionSheet(i) {
-		if(!this.state.passwords[i]) return;
-		
-		let note = this.state.passwords[i];
-		
-		this.setState({ popout: 
-			<ActionSheet
-				onClose={() => this.setState({ popout: null })}
-				title={`Аккаунт «${note.login}»`}
-				text="Действия с аккаунтом"
-			>
-				<ActionSheetItem autoclose>Скопировать пароль</ActionSheetItem>
-				<ActionSheetItem autoclose>Редактировать</ActionSheetItem>
-				<ActionSheetItem autoclose theme="destructive">Удалить</ActionSheetItem>
-				{platform() === IOS && <ActionSheetItem autoclose theme="cancel">Отмена</ActionSheetItem>}
-			</ActionSheet>
-		});
+	changeView(activeView) {
+		if(allowed[activeView]) {
+			this.setState({ activeView });
+		}
 	}
 
 	render() {
 		return (
-			<View activePanel={this.state.activePanel} popout={this.state.popout}>
-				<Home
-					id="home"
-					fetchedUser={this.state.fetchedUser}
-					go={this.go}
-					passwords={this.state.passwords}
-					onSearch={this.onSearch}
-					search={this.state.search}
-					result={this.passwords}
-					openAbout={this.openAbout}
-					openActionSheet={this.openActionSheet}
-				/>
-				<Persik id="persik" go={this.go} />
-			</View>
+			<Root activeView={this.state.activeView}>
+				<View activePanel={this.state.mainPanel} id="main">
+					<MainHome id="home" fetchedUser={this.state.fetchedUser} go={this.go} changeView={this.changeView} />
+				</View>
+				<View activePanel={this.state.addingPanel} id="adding">
+					<Adding id="home" fetchedUser={this.state.fetchedUser} go={this.go} changeView={this.changeView} />
+				</View>
+			</Root>
 		);
 	}
 }
